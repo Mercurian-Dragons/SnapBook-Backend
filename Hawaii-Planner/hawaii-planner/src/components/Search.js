1c29e077e51242d2c94c0016534cac8357c9f20a
ckntucker <ckntucker@gmail.com>
2022-07-18 13:18:29 -1000

    search input fetches correctly

--- Hawaii-Planner/hawaii-planner/src/components/Search.js
+++ Hawaii-Planner/hawaii-planner/src/components/Search.js
@@ -4,7 +4,7 @@ import SearchResults from './SearchResults';
 
 const Search = ({searchParams}) => {
     const[features, setFeatures] = useState()
-    const [searchFeature, setSearchCity] = useState('')
+    const [searchFeature, setSearchCity] = useState('honolulu')
     const searchOptions ={
         key: process.env.REACT_APP_CTPLANNER_KEY,
         // api: 'https://api.opentripmap.com/0.1/en/places/radius?radius=100000&lon=-157.834549&lat=21.276218&kinds=tourist_facilities%2Cinteresting_places&rate=2&apikey='
@@ -17,17 +17,18 @@ const Search = ({searchParams}) => {
     }, [])
 
     function getPlaces(searchFeature){
-        const url =`${searchOptions.geoApi}/geoname?apikey=${searchOptions.key}&name=waikiki`
-        fetch(`${searchOptions.geoApi}/geoname?apikey=${searchOptions.key}&name=waikiki`)
+        // const url =`${searchOptions.geoApi}/geoname?apikey=${searchOptions.key}&name=waikiki`
+        fetch(`${searchOptions.geoApi}/geoname?apikey=${searchOptions.key}&name=${searchFeature}`)
         .then(responsed => responsed.json())
         .then(responsed => {
-            // console.log(responsed.lon)
-            fetch(`${searchOptions.geoApi}radius?radius=100000&lon=${responsed.lon}&lat=${responsed.lat}&kinds=tourist_facilities%2Cinteresting_places&rate=3&apikey=${searchOptions.key}`)
+            console.log(searchFeature)
+            fetch(`${searchOptions.geoApi}radius?radius=100000&lon=${responsed.lon}&lat=${responsed.lat}&kinds=tourist_facilities%2Cinteresting_places&rate=3&limit=50&apikey=${searchOptions.key}`)
             .then(response => response.json())
             .then(response => {
                 console.log(response)
                 setFeatures(response.features)
             })
+            .catch(console.err)
         })
     }
 
@@ -39,6 +40,7 @@ const Search = ({searchParams}) => {
 
     function handleSubmit(event){
         event.preventDefault()
+        getPlaces(searchFeature)
         console.log(searchFeature)
 
     }
@@ -48,7 +50,7 @@ const Search = ({searchParams}) => {
         <div>
             <form onSubmit={handleSubmit}>
                 <label htmlFor='searchCity'>Search City </label>
-                <input type="text" id="searchCity" name="searchCity" onChange={handleChange}/>
+                <input type="text" id="searchCity" name="searchCity" placeholder='Search' required onChange={handleChange} value={searchFeature}/>
                 <button type="submit">Search</button>
         </form>
         {features&&<SearchResults features={features}/>}

5f750782daea0c32433104d44bc0118d570089ea
stvnlee890 <stvnlee890@gmail.com>
2022-08-04 20:15:01 -0700

    fixed url issue, displayed broken image

--- Image-Cup/app.js
+++ Image-Cup/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const app = express()
+const cors = require('cors')
 require('dotenv').config
 require('./db/connection')
-const app = express()
-app.set('port', process.env.PORT || 3000)
+
+app.set('port', process.env.PORT || 4000)
 const mongoose = require('mongoose')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cors())
 
 app.get('/', (req,res) => {
     res.redirect('/photo')

d6bd50bf091ee540911bfb3f3283550372c0572a
lmadkins <louise.m.adkins@gmail.com>
2022-08-10 19:15:54 -0700

    Edited README and minor code housekeeping

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
 // app.get('/', (req,res) => {
-//     res.redirect('/')
+//     res.redirect('/albums')
 // })
 
 const albumController = require('./controllers/albumController')
